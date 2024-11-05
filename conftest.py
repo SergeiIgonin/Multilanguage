856e6e6572f661f3b48f753e571021029104d154
SergeiIgonin <igonin.sergei3@gmail.com>
2024-11-05 21:44:10 +0300

    first headless action

--- conftest.py
+++ conftest.py
@@ -15,6 +15,7 @@ def browser(request):
     print("\nstart chrome browser for test..")
     options = webdriver.ChromeOptions()
     options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+    options.add_argument('--headless')
     browser = webdriver.Chrome(options=options)
     yield browser
     print("\nquit browser..")
